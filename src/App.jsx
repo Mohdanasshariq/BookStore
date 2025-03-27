e544491cfe1116dbeca5c00e7347aad9def9d5e1
Mohdanasshariq <mohdanasshariq@gmail.com>
2025-03-28 01:49:43 +0530

    fd

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
-import React from "react";
-import Home from "./home/Home";
-import { Navigate, Route, Routes } from "react-router-dom";
-import Courses from "./courses/Courses";
-import Signup from "./components/Signup";
-import { Toaster } from "react-hot-toast";
-import { useAuth } from "./context/AuthProvider";
+import React from 'react'
+import Home from './home/Home'
+import Buy from './buy/Buy'
+import {Routes,Route} from 'react-router-dom'
+import Signup from './components/Signup'
 
+import Contactt from './contact/Contactt'
 function App() {
-  const [authUser, setAuthUser] = useAuth();
-  console.log(authUser);
   return (
-    <>
-      <div className="dark:bg-slate-900 dark:text-white">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/course"
-            element={authUser ? <Courses /> : <Navigate to="/signup" />}
-          />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-        <Toaster />
-      </div>
-    </>
-  );
+   <>
+   
+   <div className='dark:bg-slate-900 dark:text-white'>
+   <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/buy" element={<Buy/>}/>
+    <Route path="/signup" element={<Signup/>}/>
+    <Route path="/contact" element={<Contactt/>}/>
+   </Routes>
+   </div>
+   </>
+  )
 }
 
-export default App;
+export default App
